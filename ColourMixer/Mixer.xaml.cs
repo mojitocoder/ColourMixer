bcb649f9f300f65b4db8435b555795b5291bd5f8
Quynh Nguyen <qn@rosslynanalytics.com>
2017-01-14 12:24:59 +0000

    Colour Mixer app

---
+++ ColourMixer/Mixer.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ColourMixer
+{
+	public partial class Mixer : ContentPage
+	{
+		public Mixer()
+		{
+			InitializeComponent();
+			boxViewColour.Color = Color.Black;
+		}
+
+		public void OnColourSliderChanged(Object sender, EventArgs e)
+		{
+			var red = sliderRed.Value;
+			var green = sliderGreen.Value;
+			var blue = sliderBlue.Value;
+			boxViewColour.Color = Color.FromRgb(red, green, blue);
+		}
+	}
+}
